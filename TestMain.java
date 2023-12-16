dac94a5040e5e2ab6578ca7dc69be872f515c954
Dang Hung Phuc <danghungphucc@gmail.com>
2023-12-16 14:31:26 +0700

    finish Account test

--- TestMain.java
+++ TestMain.java
@@ -1,19 +1,27 @@
 public class TestMain {
     public static void main(String[] args) {
         // Test constructor and toString()
-        InvoiceItem inv1 = new InvoiceItem("A101", "Pen Red", 888, 0.08);
-        System.out.println(inv1);  // toString();
+        Account a1 = new Account("A101", "Tan Ah Teck", 88);
+        System.out.println(a1);  // toString();
+        Account a2 = new Account("A102", "Kumar"); // default balance
+        System.out.println(a2);
 
-        // Test Setters and Getters
-        inv1.setQty(999);
-        inv1.setUnitPrice(0.99);
-        System.out.println(inv1);  // toString();
-        System.out.println("id is: " + inv1.getId());
-        System.out.println("desc is: " + inv1.getDesc());
-        System.out.println("qty is: " + inv1.getQty());
-        System.out.println("unitPrice is: " + inv1.getUnitPrice());
+        // Test Getters
+        System.out.println("ID: " + a1.getID());
+        System.out.println("Name: " + a1.getName());
+        System.out.println("Balance: " + a1.getBalance());
 
-        // Test getTotal()
-        System.out.println("The total is: " + inv1.getTotal());
+        // Test credit() and debit()
+        a1.credit(100);
+        System.out.println(a1);
+        a1.debit(50);
+        System.out.println(a1);
+        a1.debit(500);  // debit() error
+        System.out.println(a1);
+
+        // Test transfer()
+        a1.transferTo(a2, 100);  // toString()
+        System.out.println(a1);
+        System.out.println(a2);
     }
 }
