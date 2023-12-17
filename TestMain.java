14fa37b8d463344ad10ca963ba2233d7f3880379
Dang Hung Phuc <danghungphucc@gmail.com>
2023-12-17 07:30:20 +0700

    more homework

--- TestMain.java
+++ TestMain.java
@@ -1,27 +1,20 @@
 public class TestMain {
     public static void main(String[] args) {
         // Test constructor and toString()
-        Account a1 = new Account("A101", "Tan Ah Teck", 88);
-        System.out.println(a1);  // toString();
-        Account a2 = new Account("A102", "Kumar"); // default balance
-        System.out.println(a2);
+        Date d1 = new Date(1, 2, 2014);
+        System.out.println(d1);  // toString()
 
-        // Test Getters
-        System.out.println("ID: " + a1.getID());
-        System.out.println("Name: " + a1.getName());
-        System.out.println("Balance: " + a1.getBalance());
+        // Test Setters and Getters
+        d1.setMonth(12);
+        d1.setDay(9);
+        d1.setYear(2099);
+        System.out.println(d1);  // toString()
+        System.out.println("Month: " + d1.getMonth());
+        System.out.println("Day: " + d1.getDay());
+        System.out.println("Year: " + d1.getYear());
 
-        // Test credit() and debit()
-        a1.credit(100);
-        System.out.println(a1);
-        a1.debit(50);
-        System.out.println(a1);
-        a1.debit(500);  // debit() error
-        System.out.println(a1);
-
-        // Test transfer()
-        a1.transferTo(a2, 100);  // toString()
-        System.out.println(a1);
-        System.out.println(a2);
+        // Test setDate()
+        d1.setDate(3, 4, 2016);
+        System.out.println(d1);  // toString()
     }
 }
